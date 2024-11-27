d069a484f608ce02f01a43aad358546fea067276
kamasd <kamilkrasicki1982@gmail.com>
2024-11-27 18:03:57 +0100

    added work and jail models

--- backend/models.py
+++ backend/models.py
@@ -4,3 +4,14 @@ class discord_users(models.Model):
     id = models.BigIntegerField(primary_key = True)
     balance = models.FloatField()
 
+class work(models.Model):
+    id = models.AutoField(primary_key = True)
+    text = models.TextField()
+    money = models.FloatField()
+
+class jail(models.Model):
+    user_id = models.BigIntegerField
+    bust_time = models.DateTimeField()
+    fee = models.FloatField()
+    release_time = models.DateTimeField()
+
